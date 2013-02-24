a028a09c3adbc0f8db2af6f4617cd0a241415a06
Konstantin <konstantin.schmidt.dev@gmail.com>
2013-02-24 05:20:05 +0100

    Nun ist es möglich, Avatar zu ändern.

--- application/modules/welcome/controllers/welcome.php
+++ application/modules/welcome/controllers/welcome.php
@@ -2,9 +2,29 @@
 
 class Welcome extends CI_Controller
 {
+	
 	function index()
 	{
 		$data['container'] = $this->load->view("container");
 		$this->load->view("welcome_view", $data);
+	}
+	
+	function ava()
+	{
+		$config['upload_path'] = './assets/pics/';
+		$config['allowed_types'] = 'jpg';
+		$config['file_name'] = 'ava';
+		$config['overwrite'] = TRUE;
+		
+		$this->load->library('upload', $config);
+
+		if ( ! $this->upload->do_upload())
+		{
+		redirect("welcome","refresh");
+		}
+		else
+		{
+		redirect("welcome","refresh");
+		}
 	}
 }
--- application/modules/welcome/views/welcome_view.php
+++ application/modules/welcome/views/welcome_view.php
@@ -1,5 +1,14 @@
 <div id = "home">
 	<div class = "hero-unit">
-		<img src="<?=base_url();?>assets/pics/home.jpg" class="img-polaroid" style = "width:150px">
+		<img src="<?=base_url();?>assets/pics/ava.jpg" class="img-polaroid" style = "width:150px">
+		
+<!--Avatar hochladen-->
+<?php echo form_open_multipart('welcome/ava');?>
+	<input type="file" name="userfile"/>
+	<br>
+	<input type="submit" class = "btn btn-success" value="Avatar &auml;ndern" />
+</form>
+<!--Avatar hochladen ENDE-->
+
 	</div>
 </div>
