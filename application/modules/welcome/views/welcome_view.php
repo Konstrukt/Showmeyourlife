526798a0749500c41bd93894d011ed86507e894a
Konstantin <konstantin.schmidt.dev@gmail.com>
2013-02-24 04:47:42 +0100

    Plazierung des Avatars für eine Vorstellung.
    Avatar wird später wechselbar sein.

--- application/modules/welcome/views/welcome_view.php
+++ application/modules/welcome/views/welcome_view.php
@@ -1,1 +1,5 @@
-Es beginnt.
+<div id = "home">
+	<div class = "hero-unit">
+		<img src="<?=base_url();?>assets/pics/home.jpg" class="img-polaroid" style = "width:150px">
+	</div>
+</div>
--- application/views/container.php
+++ application/views/container.php
@@ -1,1 +1,12 @@
 <link rel = "stylesheet" type = "text/css" href = "<?=base_url();?>assets/css/bootstrap.css">
+
+
+
+
+<!--CSS für Welcome / Wird später separat-->
+<style type = "text/css">
+#home
+{
+	text-align:center;
+}
+</style>
